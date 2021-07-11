f9b0bd6b50d141c63928917cf886d772f98f4968
Jordan Altomare <jaltomar@u.rochester.edu>
2021-07-11 19:45:15 -0400

    asdfasdf

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App(){
           <Nav>
             <Nav.Link className = 'navitem'  href="/" >Home</Nav.Link>
             <Nav.Link className = 'navitem' href="/about">About</Nav.Link>
-            <Nav.Link className = 'navitem' href="/services">Services</Nav.Link>
+            <Nav.Link className = 'navitem' href="/Services">Services</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
 
@@ -43,8 +43,8 @@ function App(){
 
         <Switch>
          <Route path = "/" exact component = {Home} />
-         <Route path = "/services" component = {Services} />
-         <Route path = {process.env.PUBLIC_URL + "/about"} exact component = {About} />
+         <Route path = "/static/js/Services.js" component = {Services} />
+         <Route path = "/about" exact component = {About} />
          <Route path = "/puppybasics" exact component = {PuppyBasicForm} />
          <Route path = "/upkeep" exact component = {UpkeepForm} />
          <Route path = "/tricks-agility" exact component = {TrickForm} />
