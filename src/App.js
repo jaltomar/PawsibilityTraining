cc8bcae1ad2f2ebd27f9b91b25912a10cddc29c8
Jordan Altomare <jaltomar@u.rochester.edu>
2021-07-11 18:51:49 -0400

    eadf

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App(){
 
         <Switch>
          <Route path = "/" exact component = {Home} />
-         <Route path = "/services" component = {Services} />
+         <Route path = "pawsibilitytraining.com/services" component = {Services} />
          <Route path = "/about" exact component = {About} />
          <Route path = "/puppybasics" exact component = {PuppyBasicForm} />
          <Route path = "/upkeep" exact component = {UpkeepForm} />
