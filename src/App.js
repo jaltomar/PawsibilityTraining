2f7812be029ac67a8d1d503b274f20ae08ead6e0
Jordan Altomare <jaltomar@u.rochester.edu>
2021-07-11 22:48:58 -0400

    yeehaw

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ const fixHome = ()=>{
           {/* <Navbar.Brand className ='logo' href="/"><img  src = {doggo} style = {{width: '30px'}} /></Navbar.Brand> */}
           <Nav>
             <Nav.Link onClick = {fixHome} className = 'navitem'  href="/" style = {home? {color: '#091010'}: {color: '#2b5153'}} >Home</Nav.Link>
-            <Nav.Link onClick = {handleHome} className = 'navitem' href="/about">About</Nav.Link>
+            <Nav.Link onClick = {handleHome} className = 'navitem' href="/about">AboutMAN</Nav.Link>
             <Nav.Link onClick = {handleHome}className = 'navitem' href="/services">Services</Nav.Link>
             <Nav.Link onClick = {handleHome}href="/contact">Contact</Nav.Link>
           </Nav>
