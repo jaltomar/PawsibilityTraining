c57638f8b90083d6a46171895052e2ba0b1a3e3b
Jordan Altomare <jaltomar@u.rochester.edu>
2021-07-11 22:04:06 -0400

    asdfasdf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Nav, Navbar, Button, Form, FormControl} from 'react-bootstrap'
-import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './Home'
 import Services from './Services'
 import About from './About'
@@ -54,10 +54,10 @@ const fixHome = ()=>{
    
           {/* <Navbar.Brand className ='logo' href="/"><img  src = {doggo} style = {{width: '30px'}} /></Navbar.Brand> */}
           <Nav>
-            <Nav.Link onClick = {fixHome} className = 'navitem'  href="#/" style = {home? {color: '#091010'}: {color: '#2b5153'}} >Home</Nav.Link>
-            <Nav.Link onClick = {handleHome} className = 'navitem' href="#about">About</Nav.Link>
-            <Nav.Link onClick = {handleHome}className = 'navitem' href="#services">Services</Nav.Link>
-            <Nav.Link onClick = {handleHome}href="#contact">Contact</Nav.Link>
+            <Nav.Link onClick = {fixHome} className = 'navitem'  href="/" style = {home? {color: '#091010'}: {color: '#2b5153'}} >Home</Nav.Link>
+            <Nav.Link onClick = {handleHome} className = 'navitem' href="/about">About</Nav.Link>
+            <Nav.Link onClick = {handleHome}className = 'navitem' href="/services">Services</Nav.Link>
+            <Nav.Link onClick = {handleHome}href="/contact">Contact</Nav.Link>
           </Nav>
 
         </Navbar>
