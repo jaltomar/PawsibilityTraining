9b8204ceaf3b3638752881292c72219f311bcbe5
Jordan Altomare <jaltomar@u.rochester.edu>
2021-07-14 19:48:23 -0400

    fix

--- src/Forms/ThankYou.js
+++ src/Forms/ThankYou.js
@@ -4,6 +4,7 @@ import beautydog from '../img/beautydog.jpeg'
 
 
 function ThankYou(){
+    window.scrollTo(0,0)
     return(
         <div style = {{width: '50%', margin: 'auto', paddingTop: '40px', textAlign: 'center'}}>
             <h2 style = {{color: '#482728'}}>Thank you!  Your submission was received.  You will be contacted shortly about setting up a session.</h2>
