93b78acfde05b8c37df42001f0318b1f126e3402
Jordan Altomare <jaltomar@u.rochester.edu>
2021-07-14 23:38:40 -0400

    fix home for iphone

--- src/Home.js
+++ src/Home.js
@@ -23,18 +23,22 @@ class Home extends React.Component{
                     <img className = 'homepic' width= '100%' src = {goldie} alt = "goldie"/>
                 </div>
                 <br/>
+
+
+           
                 <p className = 'myText'> Do you find yourself frustrated with your furry friend? Are there days when your dog pushes all of your buttons? Is your puppy terrorizing you with her razor sharp teeth? Are you having trouble teaching your dog to use the outside potty? Do you want your dog to learn unique and fun tricks? Let us help you and your dog grow together. </p>
+                
                <br/><img src = {dogpic} className = 'hickFaith' alt = "faith and dog pic" /><br/>
                <p className = 'myText'><h3 style = {{textAlign: 'center'}}>Private, individualized training</h3>
                Pawsibility Training works to meet you and your dog’s specific needs. With our individualized and private 1-on-1 sessions, you will learn how to better work with your dog and practice techniques to improve their behavior. Through positive reinforcement training, we can teach your dog, and encourage desired behaviors while eliminating unwanted behaviors.</p>
-                <br/>
+                {/* <br/> */}
 
 
 
                 {/* <div className = 'blackbox'> */}
                     <div className = 'multpic'>
                         <div className= 'rowpic'> 
-                            <img className = 'pic' src = {dexanddog} alt = 'dexanddog'/>
+                            <img className = 'pic'  src = {dexanddog} alt = 'dexanddog'/>
                         </div>
                         <div className= 'rowpic'>
                             <img className = 'pic' src = {faithinpen} alt = 'faithinpen'/>
@@ -49,9 +53,9 @@ class Home extends React.Component{
 
                 <br/>
 
-                <div>
+                <div style = {{textAlign: 'center'}}> 
 
-                <div style = {{fontSize: '18pt', display: 'inline-block', verticalAlign: 'top', marginRight: '10%',  textAlign: 'center'}}>
+                <div style = {{fontSize: '18pt', marginBottom: '20px', display: 'inline-block', verticalAlign: 'top', marginRight: '10%', marginLeft: '10%', textAlign: 'center', border: 'red solid 0px'}}>
 
           
                 <h3>Expertise</h3>
@@ -79,7 +83,7 @@ class Home extends React.Component{
                 </div>
 
 
-            <div style = {{display: 'inline-block', width: '40%', verticalAlign: 'top', textAlign: 'center'}}>
+            <div style = {{display: 'inline-block', width: '40%', minWidth: '300px', verticalAlign: 'top', textAlign: 'center', border: 'red solid 0px'}}>
 
          
                 <p style = {{fontSize: '16pt', textAlign: 'left'}}>   
@@ -97,9 +101,19 @@ Before we can create an individualized plan to help you and your dog, we want to
             <br/><br/><br/>
 
 
-            
-            <p style = {{fontSize: '16pt'}}> Ready to start? Check out our <a href = '#services'> services</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Got questions? <a href = '#contact'> Contact us</a>
-            </p>
+            <div width = '60%' style ={{minWidth: '400px'}}>
+
+                <div style = {{display: 'inline-block', padding: '0 20px 0 20px', textAlign: 'left'}}>
+                <p style = {{fontSize: '16pt', display: 'inline' }}> Ready to start? Check out our <a href = '#services'> services</a></p>
+                </div>
+
+                <div style = {{display: 'inline-block', padding: '0 20px 0 20px', textAlign: 'left'}}>
+                <p style = {{fontSize: '16pt', display: 'inline'}}>
+                Got questions? <a href = '#contact'> Contact us</a>
+                </p>
+                </div>
+
+            </div>
 
 
 
