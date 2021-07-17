70d549bde79cf749ac4b195e1ec459e8d4c6e3d0
Jordan Altomare <jaltomar@u.rochester.edu>
2021-07-17 06:32:31 -0400

    is this the last one?

--- src/Forms/ThankYou.js
+++ src/Forms/ThankYou.js
@@ -16,9 +16,10 @@ useEffect(()=>{
 
     window.scrollTo(0,0)
     return(
-        <div style = {{width: '50%', margin: 'auto', paddingTop: '40px', textAlign: 'center'}}>
+        <div style = {{width: '100%', margin: 'auto', paddingTop: '40px',maxWidth: '800px', textAlign: 'center'}}>
             <h2 style = {{color: '#482728'}}>Thank you!  Your submission was received.  You will be contacted shortly about setting up a session.</h2>
-            <img width = "400px" src = {beautydog} alt = 'beautydog'/>
+            <img width = "50%" style = {{minWidth: '350px'}} src = {beautydog} alt = 'beautydog'/>
+            <br/>    <br/>
        </div>
     )
 }
