773895ba16e60c0812734cd548c0216c3103477e
Jordan Altomare <jaltomar@u.rochester.edu>
2021-07-17 03:09:57 -0400

    made major changes to try to make forms phone ready

--- src/Forms/GenericForm.js
+++ src/Forms/GenericForm.js
@@ -105,19 +105,49 @@ class GenericForm extends React.Component{
             <div className  = 'formdiv' style = {{  color: '#482728'}}>
 
                 <form onSubmit = {this.handleSubmit}>
+
+
+                 <div style = {{border: 'red solid 2px'}}>
                   <label>Full Name:</label> <input type = 'text' className = 'inputtext' name = 'fname' value = {this.state.fname} onChange = {this.handleChange}/><br/>
-                  <label>Phone Number:</label> <input type = 'text' className = 'inputtext' name = 'phone' value = {this.state.phone} onChange = {this.handleChange}/><br/>
+                  <label>Phone:</label> <input type = 'text' className = 'inputtext' name = 'phone' value = {this.state.phone} onChange = {this.handleChange}/><br/>
                   <label>Email:</label> <input type = 'text' className = 'inputtext' name = 'email' value = {this.state.email} onChange = {this.handleChange}/> <br/>
                   <label>Address:</label> <input type = 'text' className = 'inputtext' name = 'address' value = {this.state.address} onChange = {this.handleChange}/><br/>
-                  <label>Best way and time to contact:</label> <input type = 'text' className = 'inputtext' name = 'bestway' value = {this.state.bestway} onChange = {this.handleChange}/><br/>
-                  <label>How did you hear about Pawsibility Dog Training?</label> <input type = 'text' className = 'inputtext' name = 'howhear' value = {this.state.howhear} onChange = {this.handleChange}/><br/>
-              
-                  <label>What kind of dog(s) do you have? How old are they? Are they spayed/neutered?</label><br/>
-                  <textarea rows = '3' cols = '80' name = 'whatkind' value = {this.state.whatkind} onChange = {this.handleChange} /><br/>
+                  <br/><br/>
 
-                  <label>How long have you had your dog(s)? </label> <input type = 'text' className = 'inputtext' name = 'howlong' value = {this.state.howlong} onChange = {this.handleChange}/><br/>
-                  <label>Is your dog crate trained? </label> <input type = 'text' className = 'inputtext' name = 'cratetrained' value = {this.state.cratetrained} onChange = {this.handleChange}/><br/>
-                  <label>What would you like to work on with your dog(s) (select all that apply)?</label><br/> 
+                  
+                </div>
+                  
+                  
+                  <label>What is the best way and time to contact you?</label><br/> <input type = 'text' className = 'inputtext' name = 'bestway' value = {this.state.bestway} onChange = {this.handleChange}/><br/>
+                  <label>How did you hear about Pawsibility Training?</label><br/> <input type = 'text' className = 'inputtext' name = 'howhear' value = {this.state.howhear} onChange = {this.handleChange}/><br/>
+              
+
+
+
+                  <label>What kind of dog(s) do you have? How old are they?<br/> Are they spayed/neutered?</label><br/>
+
+
+
+
+
+
+
+                <div style = {{ border: 'green solid 2px', textAlign: 'left', display: '-block', width: '100%', maxWidth: '600px'}}>
+                  <textarea style = {{fontSize: '18px', width: '100%', height: '100px'}}name = 'whatkind' value = {this.state.whatkind} onChange = {this.handleChange} /><br/>
+
+                </div>
+
+
+
+
+
+
+
+
+
+                  <label>How long have you had your dog(s)? </label> <br/><input type = 'text' className = 'inputtext' name = 'howlong' value = {this.state.howlong} onChange = {this.handleChange}/><br/>
+                  <label>Is your dog crate trained? </label> <br/><input type = 'text' className = 'inputtext' name = 'cratetrained' value = {this.state.cratetrained} onChange = {this.handleChange}/><br/>
+                  <label style = {{display: 'inline-block', width: '50%'}}>What would you like to work on with your dog(s)? (select all that apply)</label><br/> 
                   <label>
                     <input type = 'radio' name = 'chewinghabits' value = 'Chewing Habits' onClick = {this.handleChange} checked = {this.state.chewinghabits === 'Chewing Habits, '} />
                     Chewing Habits
@@ -168,15 +198,46 @@ class GenericForm extends React.Component{
                     <input type = 'radio' value = 'Other' name = 'other' onClick = {this.handleChange} checked = {this.state.other === 'Other, '} />
                     Other (please describe below)
                   </label><br/>
-                  <label>Please explain a little bit about your dog(s) and areas you would like to work on...
- </label><br/> <textarea rows = '3' cols = '80' name = 'pleaseexplain' value = {this.state.pleaseexplain} onChange = {this.handleChange}/> <br/>
-                  <label>Who lives in your household and how old are they? What is their relationship to the dog(s)?
- </label><br/> <textarea rows = '3' cols = '80' name = 'wholives' value = {this.state.wholives} onChange = {this.handleChange}/><br/>
-                  <label>Have you ever used a dog trainer/behaviorist/psychologist before? Please share your experiences.
- </label> <br/> <textarea rows = '3' cols = '80' name = 'haveyou' value = {this.state.haveyou} onChange = {this.handleChange}/><br/>
-                  <label>Questions/additional information:</label> <br/> <textarea rows = '3' cols = '80' name = 'questions' value = {this.state.questions} onChange = {this.handleChange}/><br/>
 
-                  <p>Note:  All training starts with a half an hour consult to learn more about your dog ($50)</p>
+
+
+                  <div style = {{ border: 'green solid 2px', textAlign: 'left', display: 'block', width: '100%', maxWidth: '600px'}}>
+                  <label>Please explain a little bit about your dog(s) and areas you would like to work on...
+ </label><br/> <textarea style = {{fontSize: '18px', width: '100%', height: '160px'}}  name = 'pleaseexplain' value = {this.state.pleaseexplain} onChange = {this.handleChange}/> <br/>
+                 </div>
+                 
+                 
+                 
+                 
+                 <div style = {{ border: 'green solid 2px', textAlign: 'left', display: 'block', width: '100%', maxWidth: '600px'}}>
+                  <label>Who lives in your household and how old are they? What is their relationship to the dog(s)?
+ </label><br/> <textarea style = {{fontSize: '18px', width: '100%', height: '160px'}}  name = 'wholives' value = {this.state.wholives} onChange = {this.handleChange}/><br/>
+                 </div>
+                 
+                 
+                 
+                 
+                 
+                 <div style = {{ border: 'green solid 2px', textAlign: 'left', display: 'block', width: '100%', maxWidth: '600px'}}>
+                  <label>Have you ever used a dog trainer/behaviorist/psychologist before? Please share your experiences.
+ </label> <br/> <textarea style = {{fontSize: '18px', width: '100%', height: '160px'}} name = 'haveyou' value = {this.state.haveyou} onChange = {this.handleChange}/><br/>
+                 </div>
+                 
+                 
+                 
+                 
+                 
+                 
+                 <div style = {{ border: 'green solid 2px', textAlign: 'left', display: 'block', width: '100%', maxWidth: '600px'}}>
+                  <label>Questions/additional information:</label> <br/> <textarea style = {{fontSize: '18px', width: '100%', height: '160px'}} name = 'questions' value = {this.state.questions} onChange = {this.handleChange}/><br/>
+                  </div>
+
+
+
+
+
+
+                  <p style = {{maxWidth: '400px'}}>Note:  All training starts with a half an hour consult to learn more about your dog ($50)</p>
                   <div style = {{textAlign: 'center'}}> 
                      <Button type = 'submit' className = 'mybutton'>Submit</Button><br/>
                   </div>
